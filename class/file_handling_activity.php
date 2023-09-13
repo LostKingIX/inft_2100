11403f84b343a011284ef5dc7dc82e340498da35
Kuldeep M <kuldeepmohanta99@gmail.com>
2023-09-13 17:18:38 -0400

    updated file handling notes

---
+++ class/file_handling_activity.php
@@ -0,0 +1,38 @@
+<?php
+    // Author: Kuldeep Mohanta
+    // Date: September 12, 2023
+    // Description: File handling example containing two parts: practice 1 using PHP to read and write data + practice 2 using other writing modes
+?>
+
+<?php
+    // Practice 1 - read and write PHP data
+    /*
+    Create a new file
+    Write some data into it
+    Read data from the file
+    Display/echo data
+    */
+
+    // Create a file and append information to it
+    $bmw_information = fopen('./car_models.txt', 'a');
+
+    // Writing into the new file
+    fwrite($bmw_information, "BMW Models\n\n3 series are as follows:\n323i\n330i\n340i\nM340i");
+
+    // Reading and displaying information from the file
+    $bmw_information = fopen('car_models.txt', 'r'); // Open the file for reading
+    echo fread($bmw_information, filesize('car_models.txt')); // Read and echo the file contents
+    fclose($bmw_information); // Close the file stream
+?>
+
+<?php
+
+    // Practice 2 - different approaches
+    /*
+    Using fwrite(), fputs()
+    Using fread(), fgets()
+    Append mode, append+ mode
+    using feof (end of file verification)
+    */
+
+    
