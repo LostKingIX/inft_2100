8cd136c8785bdbbb8e807f18fd864ae457b5ca05
LostKingIX <kuldeepmohanta99@gmail.com>
2023-10-08 23:19:30 -0400

    updating progress on web labs

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$title = "INFT 2100 - Home Page";
-include "./includes/header.php";
+    $title = "INFT 2100 - Home Page";
+    include "./includes/header.php";
+
+    # Checking if sign-in has successfully occurred
+    
 ?>
 
 <h1 class="cover-heading">
@@ -10,7 +13,8 @@ include "./includes/header.php";
 </h1>
 
 <p class="lead">
-    This index page is the landing page for INFT 2100 where you can access a sign in page, main dashboard, as well as other links and data.
+    This index page is the landing page for INFT 2100 where you can access a sign in page, 
+    main dashboard, as well as other links and data.
 </p>
 
 <p class="lead"> = <a href="#" class="btn btn-lg btn-secondary">
